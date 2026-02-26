9f3342b69821c024dfd44416032098819a851b4e
zengin0201 <aleksejborodin711@gmail.com>
2026-02-26 18:50:24 +0300

    Update table.js

--- src/components/table.js
+++ src/components/table.js
@@ -10,14 +10,41 @@ import {cloneTemplate} from "../lib/utils.js";
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
+    let before_reverse = [...before].reverse()
+    before_reverse.forEach(key=>{
+        root[key] = cloneTemplate(key)
+        root.container.prepend(root[key].container)
+    })
+    after.forEach(key=>{
+        root[key] = cloneTemplate(key)
+        root.container.append(root[key].container)
+    })
+
+    root.container.addEventListener("change",()=> onAction())
+    root.container.addEventListener("reset",()=>{
+        setTimeout(()=> onAction(),0)
+    })
+    root.container.addEventListener("submit",(e)=>{
+        e.preventDefault()
+        onAction(e.submitter)
+    })
+    
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
 
     const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item =>{
+            const row = cloneTemplate(rowTemplate)
+            Object.keys(item).forEach(key =>{
+                if(row.elements[key]){
+                    row.elements[key].textContent = item[key]
+                }
+            })
+            return row.container
+        })
+        
         root.elements.rows.replaceChildren(...nextRows);
     }
 
