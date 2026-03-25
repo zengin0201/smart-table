0cb4c8efac48e87c8ca4202e56d2f8b274027684
Alexey Borodin <aleksejborodin711@gmail.com>
2026-03-25 22:29:34 +0300

    Update table.js

--- src/components/table.js
+++ src/components/table.js
@@ -1,50 +1,52 @@
-import { cloneTemplate } from "../lib/utils.js";
+import {cloneTemplate} from "../lib/utils.js";
 
+/**
+ * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
+ *
+ * @param {Object} settings
+ * @param {(action: HTMLButtonElement | undefined) => void} onAction
+ * @returns {{container: Node, elements: *, render: render}}
+ */
 export function initTable(settings, onAction) {
-    const { tableTemplate, rowTemplate, before, after } = settings;
+    const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
+    let before_reverse = [...before].reverse()
+    before_reverse.forEach(key=>{
+        root[key] = cloneTemplate(key)
+        root.container.prepend(root[key].container)
+    })
+    after.forEach(key=>{
+        root[key] = cloneTemplate(key)
+        root.container.append(root[key].container)
+    })
+
+    root.container.addEventListener("change",()=> onAction())
+    root.container.addEventListener("reset",()=>{
+        setTimeout(()=> onAction(),0)
+    })
+    root.container.addEventListener("submit",(e)=>{
+        e.preventDefault()
+        onAction(e.submitter)
+    })
     
-    [...before].reverse().forEach(key => {
-        root[key] = cloneTemplate(key);
-        root.container.prepend(root[key].container);
-    });
 
-    after.forEach(key => {
-        root[key] = cloneTemplate(key);
-        root.container.append(root[key].container);
-    });
+    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
 
-    root.container.addEventListener("submit", (e) => {
-        e.preventDefault();
-        onAction(e.submitter);
-    });
-
-    root.container.addEventListener("reset", (e) => {
-        e.preventDefault();
-        const form = e.target.closest('form');
-        form.reset();
-        onAction(null);
-    });
-
-    root.container.addEventListener("change", (e) => {
-        if (e.target.name === 'rowsPerPage' || e.target.name === 'page' || e.target.name === 'seller') {
-            onAction(e.target);
-        }
-    });
+    // @todo: #1.3 —  обработать события и вызвать onAction()
 
     const render = (data) => {
-        if (root.elements.rows) {
-            root.elements.rows.innerHTML = '';
-            const rows = data.map(item => {
-                const row = cloneTemplate(rowTemplate);
-                Object.keys(item).forEach(key => {
-                    if (row.elements[key]) row.elements[key].textContent = item[key];
-                });
-                return row.container;
-            });
-            root.elements.rows.append(...rows);
-        }
-    };
+        const nextRows = data.map(item =>{
+            const row = cloneTemplate(rowTemplate)
+            Object.keys(item).forEach(key =>{
+                if(row.elements[key]){
+                    row.elements[key].textContent = item[key]
+                }
+            })
+            return row.container
+        })
+        
+        root.elements.rows.replaceChildren(...nextRows);
+    }
 
-    return { ...root, render };
+    return {...root, render};
 }
