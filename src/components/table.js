6251a8d011a0f53e635791473c15a259f96bae45
Alexey Borodin <aleksejborodin711@gmail.com>
2026-03-25 20:37:04 +0300

    Refactor initTable function for clarity and efficiency

--- src/components/table.js
+++ src/components/table.js
@@ -1,52 +1,42 @@
 import {cloneTemplate} from "../lib/utils.js";
 
-/**
- * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
- *
- * @param {Object} settings
- * @param {(action: HTMLButtonElement | undefined) => void} onAction
- * @returns {{container: Node, elements: *, render: render}}
- */
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
-    let before_reverse = [...before].reverse()
-    before_reverse.forEach(key=>{
-        root[key] = cloneTemplate(key)
-        root.container.prepend(root[key].container)
-    })
-    after.forEach(key=>{
-        root[key] = cloneTemplate(key)
-        root.container.append(root[key].container)
-    })
-
-    root.container.addEventListener("change",()=> onAction())
-    root.container.addEventListener("reset",()=>{
-        setTimeout(()=> onAction(),0)
-    })
-    root.container.addEventListener("submit",(e)=>{
-        e.preventDefault()
-        onAction(e.submitter)
-    })
     
+    [...before].reverse().forEach(key => {
+        root[key] = cloneTemplate(key);
+        root.container.prepend(root[key].container);
+    });
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    after.forEach(key => {
+        root[key] = cloneTemplate(key);
+        root.container.append(root[key].container);
+    });
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener("change", () => onAction());
+    root.container.addEventListener("reset", () => {
+        setTimeout(() => onAction(), 0);
+    });
+    root.container.addEventListener("submit", (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    });
 
     const render = (data) => {
-        const nextRows = data.map(item =>{
-            const row = cloneTemplate(rowTemplate)
-            Object.keys(item).forEach(key =>{
-                if(row.elements[key]){
-                    row.elements[key].textContent = item[key]
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => {
+                if (row.elements[key]) {
+                    row.elements[key].textContent = item[key];
                 }
-            })
-            return row.container
-        })
+            });
+            return row.container;
+        });
         
-        root.elements.rows.replaceChildren(...nextRows);
-    }
+        root.elements.body.innerHTML = '';
+        root.elements.body.append(...nextRows);
+    };
 
-    return {...root, render};
+    return { ...root, render };
 }
