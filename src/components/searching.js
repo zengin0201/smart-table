ad769cd365707bceb894304c882073e4592452b9
Alexey Borodin <aleksejborodin711@gmail.com>
2026-03-25 20:36:29 +0300

    Change initSearching to return a constant true function
    Refactor initSearching function to always return true.

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,13 +1,3 @@
 export function initSearching() {
-    return (vseDannie, pamyat, sobitie) => {
-        const iskomiyTekst = pamyat.search ? pamyat.search.toLowerCase() : '';
-        
-        if (!iskomiyTekst) return vseDannie;
-
-        return vseDannie.filter(stroka => {
-            const prodavec = stroka.seller ? stroka.seller.toLowerCase() : '';
-            const pokupatel = stroka.customer ? stroka.customer.toLowerCase() : '';
-            return prodavec.includes(iskomiyTekst) || pokupatel.includes(iskomiyTekst);
-        });
-    }
+    return () => true;
 }
