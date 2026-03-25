6bd18e8dfc99591186f83b42f11e8a3919ce7d31
Alexey Borodin <aleksejborodin711@gmail.com>
2026-03-25 20:35:22 +0300

    Refactor initData to support async data fetching
    Refactor initData function to fetch sellers and customers asynchronously, and handle unknown sellers and customers gracefully.

--- src/data.js
+++ src/data.js
@@ -1,14 +1,58 @@
 import {makeIndex} from "./lib/utils.js";
 
 export function initData(sourceData) {
-    const sellers = makeIndex(sourceData.sellers, 'id', v => `${v.first_name} ${v.last_name}`);
-    const customers = makeIndex(sourceData.customers, 'id', v => `${v.first_name} ${v.last_name}`);
-    const data = sourceData.purchase_records.map(item => ({
+    const BASE_URL = 'https://webinars.webdev.education-services.ru/sp7-api';
+    
+    let sellers;
+    let customers;
+    let lastResult;
+    let lastQuery;
+
+    const mapRecords = (data) => data.map(item => ({
         id: item.receipt_id,
         date: item.date,
-        seller: sellers[item.seller_id],
-        customer: customers[item.customer_id],
+        seller: sellers[item.seller_id] || 'Unknown',
+        customer: customers[item.customer_id] || 'Unknown',
         total: item.total_amount
     }));
-    return {sellers, customers, data};
+
+    const getIndexes = async () => {
+        if (!sellers || !customers) {
+            const [sellersData, customersData] = await Promise.all([
+                fetch(`${BASE_URL}/sellers`).then(res => res.json()),
+                fetch(`${BASE_URL}/customers`).then(res => res.json()),
+            ]);
+            
+            sellers = makeIndex(sellersData, 'id', v => `${v.first_name} ${v.last_name}`);
+            customers = makeIndex(customersData, 'id', v => `${v.first_name} ${v.last_name}`);
+        }
+        return { sellers, customers };
+    };
+
+    const getRecords = async (query, isUpdated = false) => {
+        const qs = new URLSearchParams();
+        Object.entries(query).forEach(([key, value]) => {
+            if (value !== undefined && value !== null && value !== '') {
+                qs.append(key, value);
+            }
+        });
+
+        const nextQuery = qs.toString();
+        if (lastQuery === nextQuery && !isUpdated) {
+            return lastResult;
+        }
+
+        const response = await fetch(`${BASE_URL}/records?${nextQuery}`);
+        const records = await response.json();
+
+        lastQuery = nextQuery;
+        lastResult = {
+            total: records.total,
+            items: mapRecords(records.items)
+        };
+
+        return lastResult;
+    };
+
+    return { getIndexes, getRecords };
 }
