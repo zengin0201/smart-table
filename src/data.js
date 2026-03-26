9ed0d9e45f9f6405777d960a17b89c9db6716fc5
Alexey Borodin <aleksejborodin711@gmail.com>
2026-03-26 10:29:36 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,21 +1,4 @@
-export function initData() {
-    const BASE_URL = 'https://webinars.webdev.education-services.ru/sp7-api';
-    let sellers = null;
-    let customers = null;
-    let lastResult = null;
-    let lastQuery = null;
-
-    const mapRecords = (data) => data.map(item => ({
-        id: item.receipt_id,
-        date: item.date,
-        seller: sellers ? (sellers[item.seller_id] || item.seller_id) : item.seller_id,
-        customer: customers ? (customers[item.customer_id] || item.customer_id) : item.customer_id,
-        total: item.total_amount
-    }));
-
-    const getIndexes = async () => {
-        if (!sellers || !customers) {
-            const [sData, cData] = await Promise.all([const BASE_URL = 'https://webinars.webdev.education-services.ru/sp7-api';
+const BASE_URL = 'https://webinars.webdev.education-services.ru/sp7-api';
 
 let sellers;
 let customers;
@@ -65,41 +48,4 @@ export function initData() {
         getIndexes,
         getRecords
     };
-}
-                fetch(`${BASE_URL}/sellers`).then(res => res.json()),
-                fetch(`${BASE_URL}/customers`).then(res => res.json()),
-            ]);
-            sellers = sData;
-            customers = cData;
-        }
-        return { sellers, customers };
-    };
-
-    const getRecords = async (query, isUpdated = false) => {
-        const qs = new URLSearchParams();
-        Object.entries(query).forEach(([key, value]) => {
-            if (value !== undefined && value !== null && value !== '') {
-                qs.append(key, value);
-            }
-        });
-        
-        const nextQuery = qs.toString();
-
-        if (lastQuery === nextQuery && !isUpdated && lastResult) {
-            return lastResult;
-        }
-
-        const response = await fetch(`${BASE_URL}/records?${nextQuery}`);
-        const records = await response.json();
-
-        lastQuery = nextQuery;
-        lastResult = {
-            total: records.total,
-            items: mapRecords(records.items)
-        };
-
-        return lastResult;
-    };
-
-    return { getIndexes, getRecords };
 }
